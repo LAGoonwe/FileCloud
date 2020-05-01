7ea50ec4e87e6f6b3dfa150b0249f4b757721b7c
briancen(岑浩臻) <briancen@tencent.com>
2020-05-01 19:57:54 +0800

    增加动态路径配置

---
+++ config/path.go
@@ -0,0 +1,9 @@
+package config
+
+const (
+	// 前端静态资源路径
+	StaticPath = "C:\\Users\\briancen\\Desktop\\workspace\\go\\src\\FileCloud"
+
+	// 上传文件本地存放路径
+	UploadPath = "C:\\Users\\briancen\\Desktop\\workspace\\go\\src\\FileCloud\\static\\files"
+)
--- handler/test.go
+++ handler/test.go
@@ -1,5 +1,5 @@
-package main
+package handler
 
 func main() {
-	$END$
+
 }
